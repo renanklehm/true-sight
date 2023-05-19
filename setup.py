5f3d1beee0f61584cac939c56a5ff76cef115d74
renanklehm <renanotivin@hotmail.com>
2023-05-19 11:00:53 -0300

    set up pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'truesight',
+    packages = ['truesight'],
+    version = 'version number',
+    description = 'Truesight is a python package for time series prediction using deep learning and statistical models.',
+    author = 'Renan Otvin Klehm',
+    author_email = 'renanotivin@hotmail.com',
+    url = 'https://github.com/renanklehm/true-sight',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
