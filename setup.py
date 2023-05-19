f720c74a81cb63b10267ebd4a5ddd086e6d706cb
Renan Klehm <renanotivin@hotmail.com>
2023-05-19 16:15:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setup(
     name = 'truesight',
     packages = ['truesight'],
-    version = 'v0.0.2a0',
+    version = 'v0.0.2a2',
     description = 'Truesight is a python package for time series prediction using deep learning and statistical models.',
     author = 'Renan Otvin Klehm',
     author_email = 'renanotivin@hotmail.com',
